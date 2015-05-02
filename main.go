d56ade1b615bf3aa1e8b594fe244028e63812717
James Lal <james@lightsofapollo.com>
2015-05-01 21:52:46 -0700

    a testing version which is dumbest and also has the best memory consumption profile

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"github.com/awslabs/aws-sdk-go/aws"
-	"github.com/awslabs/aws-sdk-go/service/s3"
-	"github.com/awslabs/aws-sdk-go/service/s3/s3manager"
+	"github.com/mitchellh/goamz/aws"
+	"github.com/mitchellh/goamz/s3"
 	"log"
 	"os"
 )
@@ -11,25 +10,25 @@ import (
 func main() {
 	bucket := os.Args[1]
 	dest := os.Args[2]
-	path := os.Args[3]
+	sourceFile := os.Args[3]
 
-	s3 := s3.New(&aws.Config{Region: "us-west-2"})
-	log.Printf("Uploading to bucket=%s key=%s with contents of %s", bucket, dest, path)
+	auth, err := aws.EnvAuth()
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := s3.New(auth, aws.USWest2)
+	log.Printf("Uploading to bucket=%s key=%s with contents of %s", bucket, dest, sourceFile)
 
-	file, err := os.Open(path)
-
+	file, err := os.Open(sourceFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	upload := s3manager.UploadInput{
-		Body:   file,
-		Bucket: &bucket,
-		Key:    &dest,
+	stat, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	uploadResult, err := s3manager.Upload(s3, &upload, nil)
-
-	log.Printf("%s", path)
-	log.Printf("%v", uploadResult)
+	bucketClient := client.Bucket(bucket)
+	bucketClient.PutReader(dest, file, stat.Size(), "text/plain", s3.PublicReadWrite)
 }
